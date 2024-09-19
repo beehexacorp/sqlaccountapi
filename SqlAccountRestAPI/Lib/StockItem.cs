ec01fcb103760f6de999c0f28e77f44d7ab00269
datnguyenbhx <duc.dat@beehexa.com>
2024-09-19 11:11:36 +0700

    [CLEAN] rm unused code

--- SqlAccountRestAPI/Lib/StockItem.cs
+++ SqlAccountRestAPI/Lib/StockItem.cs
@@ -188,43 +188,6 @@ namespace SqlAccountRestAPI.Lib
             string filePath = Path.Combine(debugFolderPath, fileName);
             var FileName = IvBizObj.Select("*", "", "Code", "AD", ",", filePath);
         }
-        public string LoadByQuery(Query query)
-        {
-            var IvBizObj = app.ComServer.BizObjects.Find(query.Type);
-
-            string xmlString = IvBizObj.Select("*", query.Where, "", "SX", ",", "");
-
-            // Convert XML to Json
-            var doc = new XmlDocument();
-            doc.LoadXml(xmlString);
-
-            var rowDataNode = doc.SelectSingleNode("//ROWDATA");
-
-            if (rowDataNode == null)
-            {
-                return "";
-            }
-
-            var jsonText = JsonConvert.SerializeXmlNode(rowDataNode, Newtonsoft.Json.Formatting.Indented, true);
-
-            var jsonObj = JObject.Parse(jsonText);
-            var rows = jsonObj["ROW"];
-            if (rows == null)
-                return "[]";
-            if (rows.Type == JTokenType.Object)
-            {
-                rows = new JArray(rows);
-            }
-
-            // Convert Row atts into key-value
-            foreach (var row in rows)
-            {
-                var propertiesToRename = row.Children<JProperty>().Where(p => p.Name.StartsWith("@")).ToList();
-                foreach (var prop in propertiesToRename)
-                    prop.Replace(new JProperty(prop.Name.TrimStart('@'), prop.Value));
-            }
-
-            return rows.ToString(Newtonsoft.Json.Formatting.Indented);
-        }
+        
     }
 }
