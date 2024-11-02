0a337bc5cda27cd9349d38c38c5c0c29ccd2050b
datnguyenbhx <duc.dat@beehexa.com>
2024-11-02 12:53:16 +0700

    rework flow & optimize user experiment

---
+++ SqlAccountRestAPI/Controllers/testDeploy.cs
@@ -0,0 +1,36 @@
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using SqlAccountRestAPI.Lib;
+using SqlAccountRestAPI.Models;
+using StockItem = SqlAccountRestAPI.Lib.StockItem;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SqlAccountRestAPI.Controllers
+{
+    [Route("/")]
+    [ApiController]
+    public class testDeploy : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult test(){
+            try
+            {
+                return Ok("Running");
+            }
+            catch (Exception ex)
+            {
+                var errorResponse = new
+                {
+                    error = ex.ToString(),
+                    code = 400
+                };
+                return BadRequest(errorResponse);
+            }
+        }
+    }
+    
+}
--- SqlAccountRestAPI/Program.cs
+++ SqlAccountRestAPI/Program.cs
@@ -8,6 +8,8 @@ builder.Logging.ClearProviders();
 builder.Logging.AddConsole(); 
 builder.Logging.AddFile("Logs/Request-{Date}.txt"); 
 
+builder.WebHost.UseIISIntegration();  // Thêm dòng này
+
 // Add services to the container.
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -15,6 +17,11 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddSingleton<SqlComServer>();
+
+// builder.WebHost.UseKestrel(options =>
+// {
+//     options.ListenAnyIP(5280); // Cổng HTTP
+// });
 
 var app = builder.Build();
 
@@ -27,10 +34,11 @@ if (app.Environment.IsDevelopment())
 
 app.UseMiddleware<RequestResponseLoggingMiddleware>();
 
-app.UseHttpsRedirection();
+// app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
 
 app.Run();
+
